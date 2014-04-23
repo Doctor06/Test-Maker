17511568679bc68ef1edbbf8b66c390191028159
Bernardo Carrasco <vivachile89@hotmail.com>
2014-04-23 17:17:05 -0600

    Created new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMaker::Application.config.secret_token = '40eb682a57216178022765507394eef8ba248d50eac8479cc26f64b289a1fdd20d7aefe243ce39e01560acddae46060870cbdbc90ac83fc16e97f59f55955364'
